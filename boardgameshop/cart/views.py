38503793f4d994e656cef05c47b1a2546449ab22
MissBlumarine <miss_blumarine01@mail.ru>
2022-11-12 21:29:07 +0300

    last_changes

--- boardgameshop/cart/views.py
+++ boardgameshop/cart/views.py
@@ -30,7 +30,7 @@ def cart_remove(request, pk):
 
 def cart_detail(request):
     cart = Cart(request)
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'cart/cart_detail.html', {'cart': cart})
 
 
 class CartDetailView(DetailView):

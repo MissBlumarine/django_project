e97af6ff990b280dc066b1d3b92e8066c0cff605
MissBlumarine <miss_blumarine01@mail.ru>
2022-11-13 10:01:31 +0300

    изменение в шаблоне заказа

--- boardgameshop/orders/urls.py
+++ boardgameshop/orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import OrderListView
 
 app_name = "orders"
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
-    # path('created_orders/', views.order_create, name='order_list'),
+    path('created_orders/', OrderListView.as_view(), name='order_list'),
     # path('created_orders/<pk:int>/', views.order_create, name='order_detail'),
 ]
--- boardgameshop/orders/views.py
+++ boardgameshop/orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.views.generic import ListView, DetailView
 
 
 def order_create(request):
@@ -25,3 +26,12 @@ def order_create(request):
     return render(request, 'orders/order/create.html',
                   {'cart': cart, 'form': form})
 
+
+class OrderListView(ListView):
+    model = Order
+    context_object_name = "orders"
+    queryset = (Order
+                .objects
+                .all()
+                )
+    template_name = 'orders/order/order_list.html'
